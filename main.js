7b802b9bc373f08ea7ea8563d4ac19f19f4c40f2
chickenCabbage <alon.shiboleth@gmail.com>
2017-07-29 23:29:09 +0300

    reverted

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var tumblrwks = require("tumblrwks");
+var sleep = require("system-sleep");
 
 var tumblr = new tumblrwks({
 	consumerKey: "5oZIvD8mfO8S61Pya3sF5a3rmPCypHX6AygHsNRmWDCiurR26B",
@@ -24,3 +25,26 @@ function postFart() {
 }
 
 console.log("Starting now.");
+
+var date;
+while(true) {
+	date = new Date();
+	var hour = date.getUTCHours();
+	var minute = date.getUTCMinutes();
+
+	//sleep system:
+
+	if(!(hour == 3 && minute >= 30) && hour != 4) sleep(1000 * 60 * 30); //if it's not 3 or 4, wait half an hour
+	else { //if it's either past 3 and a half or 4
+		if(hour == 3) { //if the hour is past 3 and a half
+			sleep(1000 * 60 * 5); //if it's past 3 and a half, wait five minutes
+		}
+		else { //if the hour is 4
+			if(minute == 10) {
+				postFart(); //if it's 4:10!
+				sleep(1000 * 60 * 60 * 21); //wait 21 hours
+			}
+			else sleep(1000 * 60 * 0.25); //if it's not 4:10 yet, wait 15 seconds
+		}
+	}
+}
